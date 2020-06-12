1d647e907786178a303b61b32fc2677a94fbda4c
Chaoyang Zhu <Chaoyang Zhu>
2020-06-12 01:18:30 -0500

    complete lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,17 @@ public class LeapYear {
         }
     }
 
+    /** Calls isLeapYear to print correct statement.
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 & year % 100 != 0)){
+	    return true;
+	}else{
+	    return false;
+	}
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
